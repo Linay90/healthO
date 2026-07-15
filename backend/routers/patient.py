dfc61dae3b43051d9ac5ca13d2717ac94dc6efb3
Linay90 <linaybharambe1108@gmail.com>
2026-07-15 20:32:07 +0530

    patient module completed

--- backend/routers/patient.py
+++ backend/routers/patient.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException 
 from schemas.patient import(
     PatientCreate,
     PatientResponse
@@ -18,7 +18,8 @@ def get_patients():
 def create_patient(patient:PatientCreate):
     new_patient={
         "id":len(patients)+1,
-        **patient.model_dump()
+        **patient.model_dump() 
+        #"** is used for creating a new dictionary by unpacking another dictionary into it."   
     }
     patients.append(new_patient)
     return new_patient
@@ -28,3 +29,31 @@ def get_patient(patient_id:int):
     for patient in patients:
         if patient["id"]==patient_id:
             return patient
+    raise HTTPException(
+        status_code=404,
+        detail="Patient not found"
+    )
+
+@router.put("/{patient_id}",response_model=PatientResponse)
+def update_patient(patient_id:int,patient:PatientCreate):
+    for existing_patient in patients:
+        if existing_patient["id"]==patient_id:
+            existing_patient.update(patient.model_dump())
+            return existing_patient
+    raise HTTPException(
+        status_code=404,
+        detail="patient not found"
+    )
+
+@router.delete("/{patient_id}")
+def delete_patient(patient_id:int):
+    for index,patient in enumerate(patients):
+        if patient["id"]==patient_id:
+            patients.pop(index)
+            return{
+                "message":f"patient-deleted-{patient_id}"
+            }
+    raise HTTPException(
+        status_code=404,
+        detail="Patient not found"
+    )
